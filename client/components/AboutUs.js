aa413b792bfdf1d394207c1cd07d2040d0317a5c
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-01-12 11:41:56 -0500

    navbar done

---
+++ client/components/AboutUs.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { connect } from "react-redux";
+import { Link } from "react-router-dom"
+
+
+
+export default class AboutUs extends React.Component {
+
+
+  render(){
+    return (
+      <div>
+        Do you ever feel like covid got you down?? That's why we built this store! Our mission is to help you green up your space and feel better. When you are stuck indoors quarantining maybe even get you some extra fresh air!
+        <p>In our website you can find all different types of plants, whether you are looking for an easy-care plant, something same for your pet, fresher air or just something pretty.</p>
+        <p>Meet our team:</p>
+        <p>Ania</p>
+        <p>Sandra</p>
+        <p>Rachel</p>
+        <p>Seldon</p>
+      </div>
+    )
+  }
+}
