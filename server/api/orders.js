3279450b9f031e6342fdd74a7d0605485eb8a638
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-01-13 20:40:30 -0500

    orderRoutes w plants and user

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const Order = require("../db/models/Order");
+const Plant = require("../db/models/Plant");
 const User = require('../db/models/User')
 
-// GET: all plants
+// GET: all orders w associated user
+// api/orders
 router.get("/", async (req, res, next) => {
   try {
     const orders = await Order.findAll({
@@ -16,6 +18,21 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+// GET: all orders w order_plant through table
+// api/orders/plants
+router.get("/plants", async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({
+      include: {
+        model: Plant
+      }
+    });
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+// api/orders/:orderId
 router.get("/:orderId", async (req, res, next) => {
   try {
     const order = await Order.findByPk(req.params.orderId);
