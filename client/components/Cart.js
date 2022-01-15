1685d4dc75e8adaeef3ffafdb2583eabdba7ac3c
Ania Marecki <aniamarecki@gmail.com>
2022-01-15 14:02:50 -0500

    my git pull never worked from before

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,23 +3,19 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import CartItem from "./CartItem";
 import addToCartThunk from "../store/cart"
+import Checkout from "./Checkout"
 
 //need to have user connected here from redux store and through the user we get the plants associated
 //use magic methods addchild to add plant onto user model.
 
 class Cart extends React.Component {
-
-
   render() {
-    console.log(this.props);
     return (
       <div className="cartscreen">
         <div className="cartscreen__left">
-          <h2>Shopping Cart</h2>
-          <CartItem />
-          <CartItem />
-          <CartItem />
-          <CartItem />
+          <h2>Your Shopping Cart</h2>
+
+          <CheckOut />
         </div>
         <div className="cartscreen__right">
           <div className="cartscreen__info">
