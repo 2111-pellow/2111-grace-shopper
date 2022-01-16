2419312ad150ee5e63de32f365d81a0ee3749ff1
Ania Marecki <aniamarecki@gmail.com>
2022-01-15 22:48:24 -0500

    functional cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -12,30 +12,37 @@ class Cart extends React.Component {
   }
 
   render() {
+      let totalPrice = 0
       let cartItems = JSON.parse(localStorage.getItem('cart'))
-  console.log(cartItems)
+      const price = (cartItems) =>{
+        for (let i=0; i < cartItems.length; i++){
+          totalPrice += parseInt(cartItems[i].price)
+        }
+        return totalPrice
+      }
+  console.log(cartItems.ImageUrl)
     return (
       <div>
       <div className="cartscreen">
         <div className="cartscreen__left">
           <h2>Your Shopping Cart</h2>
         </div>
-        <div>{cartItems.map((plant, index)=>{
+          </div>
+        <div className="cartscreen__right">
+          <div className="cartscreen__info">
+          <div>{cartItems.map((plant, index)=>{
           return (
           <div key={index}>
             <div>{plant.plant_name}</div>
-            <div>{plant.plant_ImageUrl}</div>
+            <img src={plant.ImageUrl} style={{ width: "200px", height: "200px" }}></img>
             <div>{plant.price}</div>
           </div>)
         })}
       </div>
-          </div>
-        <div className="cartscreen__right">
-          <div className="cartscreen__info">
-            {/* <p>Subtotal {cartItems.length} items</p> */}
-            <p>$500</p>
+            <p>Subtotal for your {cartItems.length} items is ${price(cartItems)}</p>
           </div>
           <div>
+
             <button>Checkout</button>
             <button onClick={()=>{window.localStorage.clear()}}>Clear Cart</button>
           </div>
